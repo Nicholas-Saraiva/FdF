dc8e48e8324a515e367da8fed99dcaf61c44cea5
CafeComLag <nicholas.saraivadev@gmail.com>
2025-07-31 11:12:40 +0100

    FIX: Fixed the bug of flip images when occur overlapping of colors

--- ft_display_image.c
+++ ft_display_image.c
@@ -6,7 +6,7 @@
 /*   By: nsaraiva <nsaraiva@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/18 17:59:32 by nsaraiva          #+#    #+#             */
-/*   Updated: 2025/07/30 17:28:33 by nsaraiva         ###   ########.fr       */
+/*   Updated: 2025/07/31 02:34:26 by nsaraiva         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -52,7 +52,7 @@ static t_3d	scale_transform(t_3d point3d, t_data *data)
 	return (result);
 }
 
-static void	ft_connect(int i, int j, t_map *map, t_data *data)
+/*static void	ft_connect(int i, int j, t_map *map, t_data *data)
 {
 	t_3d	p1;
 	t_3d	p2;
@@ -68,7 +68,7 @@ static void	ft_connect(int i, int j, t_map *map, t_data *data)
 		p2 = scale_transform(data->map->matrix[i + 1][j], data);
 		draw_line(p1, p2, data);
 	}
-}
+}*/
 
 void	apply_rotation(t_data *data)
 {
@@ -84,43 +84,82 @@ void	apply_rotation(t_data *data)
 			data->map->matrix[i][j] = ft_find_rotation(data, data->map->matrix[i][j]);
 	}
 }
+typedef struct s_line {
+	t_3d p1;
+	t_3d p2;
+	float depth; // média dos z
+} t_line;
+
+static float	get_depth(t_3d p1, t_3d p2)
+{
+	return ((p1.z + p2.z) / 2.0f);
+}
+
+static int	compare_lines(const void *a, const void *b)
+{
+	float depth_a = ((t_line *)a)->depth;
+	float depth_b = ((t_line *)b)->depth;
+
+	if (depth_a < depth_b)
+		return (-1);
+	if (depth_a > depth_b)
+		return (1);
+	return (0);
+}
+
 
 void	display_image(t_map *map, t_data *data)
 {
-	int		i;
-	int		j;
-	static double	x = 0;
+	t_line	*lines;
+	int		i, j, count = 0;
+	int		total = (map->width - 1) * map->height + (map->height - 1) * map->width;
 
+	lines = malloc(sizeof(t_line) * total);
+	if (!lines)
+		return ;
 
+	// Gerar as linhas
+	for (i = 0; i < map->height; i++)
+	{
+		for (j = 0; j < map->width; j++)
+		{
+			t_3d p = scale_transform(map->matrix[i][j], data);
+
+			if (j < map->width - 1)
+			{
+				t_3d p2 = scale_transform(map->matrix[i][j + 1], data);
+				lines[count++] = (t_line){p, p2, get_depth(p, p2)};
+			}
+			if (i < map->height - 1)
+			{
+				t_3d p2 = scale_transform(map->matrix[i + 1][j], data);
+				lines[count++] = (t_line){p, p2, get_depth(p, p2)};
+			}
+		}
+	}
+
+	// Ordenar pelas profundidades
+	qsort(lines, count, sizeof(t_line), compare_lines);
+
+	// Limpar imagem
 	ft_bzero(data->addr, 1 + WIDTH * HEIGHT * sizeof(int));
-	if (data->map->rotation.x || data->map->rotation.y || data->map->rotation.z)
-	{
+
+	// Aplicar rotação se necessário
+	if (map->rotation.x || map->rotation.y || map->rotation.z)
 		apply_rotation(data);
-		x += data->map->rotation.y;
-		printf("x: %f\n", x);
-	}
-	if (x >= -2.55 && x <= 0.50)
-	{
-		j = -1;
-		while (++j < map->width)
-		{
-			i = -1;
-			while (++i < map->height)
-				ft_connect(i, j, data->map, data);
-		}
-	}
-	else
-	{
-		j = -1;
-		while (++j < map->width)
-		{
-			i = map->height;
-			while (--i >= 0)
-				ft_connect(i, j, data->map, data);
-		}
-	}
-	data->map->rotation.x = 0;
-	data->map->rotation.y = 0;
-	data->map->rotation.z = 0;
+
+	// Desenhar as linhas ordenadas
+	for (int k = 0; k < count; k++)
+		draw_line(lines[k].p1, lines[k].p2, data);
+
+	free(lines);
+
+	// Resetar rotação
+	map->rotation.x = 0;
+	map->rotation.y = 0;
+	map->rotation.z = 0;
+
+	// Mostrar na janela
 	mlx_put_image_to_window(data->init, data->display, data->img, 0, 0);
 }
+
